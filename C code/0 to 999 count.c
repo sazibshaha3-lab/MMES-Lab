91619d42a2d3228a5d97fdeeadb9b549e49405f3
Sazib Kumar Shaha <sazibshaha3@gmail.com>
2026-04-19 20:03:38 +0600

    C code for keil

---
+++ C code/0 to 999 count.c
@@ -0,0 +1,34 @@
+#include<reg51.h>
+
+void delay(unsigned int ms)
+{
+    unsigned int i, j;
+    for(i = 0; i<ms; i++)
+        for(j = 0; j<=1383; j++);  // 11.0592 MHz
+}
+
+void main(void)
+{
+    char number[] = {0x3F, 0x06, 0x5B, 0x4F, 0x66, 0x6D, 0x7D, 0x07, 0x7F, 0x6F};
+    int hundreds, tens, ones, count;
+
+    P1 = 0x00;  // Hundreds (???)
+    P2 = 0x00;  // Tens (??????)
+    P3 = 0x00;  // Ones (???)
+
+    while(1)
+    {
+        for(count = 0; count <= 999; count++)
+        {
+            hundreds = count / 100;
+            tens     = (count % 100) / 10;
+            ones     = count % 10;
+
+            P1 = number[hundreds];
+            P2 = number[tens];
+            P3 = number[ones];
+
+            delay(50);  // 0.5 sec
+        }
+    }
+}
